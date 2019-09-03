3f9118d37f29418dd359988e0a824d91410930ae
Pongpat93 <51689283+Pongpat93@users.noreply.github.com>
2019-09-03 11:28:37 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -42,7 +42,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'remotemysql.com'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'n3LjCkthtr'),
             'username' => env('DB_USERNAME', 'n3LjCkthtr'),
