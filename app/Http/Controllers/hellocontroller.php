fa25d2a57de1bf526474470418e9152868b4341d
Pongpat93 <51689283+Pongpat93@users.noreply.github.com>
2019-10-01 09:47:26 +0700

    Update hellocontroller.php

--- app/Http/Controllers/hellocontroller.php
+++ app/Http/Controllers/hellocontroller.php
@@ -9,6 +9,4 @@ class HelloController extends Controller
    function showhello($name){
       return '<h1>hello:'.$name.' </h1>';
    }
-   function resultproduct($product){
-      return '<h1>show:'.$product.' </h1>';
-   }
+ 
