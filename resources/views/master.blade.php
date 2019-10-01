d436bab1d3d8160193bfcef853d7bcf923dca5fa
Pongpat93 <51689283+Pongpat93@users.noreply.github.com>
2019-10-01 12:23:49 +0700

    Update master.blade.php

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,14 +3,9 @@
   <head>
     <meta charset="utf-8">
     <title>@yield('title')</title>
-    <style media="screen">
-      body{
-        background-color: yellow;
-      } 
-    </style>
    </head>
    <body>
         @yield('content')
   </body>
   <footer>@yield('footer')</footer>
- </html>
+</html>
